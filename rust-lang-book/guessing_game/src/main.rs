a86581e3550c5867d4a8b85274ddadfb6b93aadc
Paul Curry <pcurry@gmail.com>
2016-12-29 03:28:55 -0500

    Guessing game MVP

---
+++ rust-lang-book/guessing_game/src/main.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();  // Test comment.
+
+    io::stdin().read_line(&mut guess)  // Apparently carriage returns don't matter.
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+
+}
