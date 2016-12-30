7c0a9dccb22cb6c81c712eb0705888bd8ea1f785
Paul Curry <pcurry@gmail.com>
2016-12-30 00:52:27 -0500

    Added the random number generator

--- rust-lang-book/guessing_game/src/main.rs
+++ rust-lang-book/guessing_game/src/main.rs
@@ -1,8 +1,14 @@
-use std::io;
+extern crate rand;
 
+use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
